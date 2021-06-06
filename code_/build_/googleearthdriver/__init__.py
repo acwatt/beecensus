1c78b46f5827f5fd544ebbce74608ddadcc3b624
Aaron Watt <aaron@acwatt.net>
2021-06-06 11:10:47 -0700

    Start googleearthdriver package

---
+++ code_/build_/googleearthdriver/__init__.py
@@ -0,0 +1,18 @@
+# Python 3.7
+# File name: main.py
+# Description: end-to-end, run all data manipulation tasks and analyses.
+# Authors: Eugenie Dugoua, Marion Dumas, Aaron Watt
+# Date: 2021-05-26
+
+# Standard library imports
+
+# Third-party imports
+
+# Local application imports
+
+# FUNCTIONS --------------------------
+
+
+# MAIN -------------------------------
+if __name__ == "__main__":
+    pass
