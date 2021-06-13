92754361104a6f8f7e2ca1d26de693e591e4ee76
Aaron Watt <aaron@acwatt.net>
2021-06-12 22:44:24 -0700

    added image_cutter to cut up large images and the bones of edit_geprint to build many geprint config files for downloading. Added TODOs

--- code_/build_/googleearthdriver/google_earth_driver.py
+++ code_/build_/googleearthdriver/google_earth_driver.py
@@ -14,8 +14,11 @@ import os
 import re
 import win32api
 from time import sleep
+from PIL import Image
+from math import floor
 
 # Third-party imports
+from image_slicer import slice
 
 # Local application imports
 
@@ -146,10 +149,64 @@ def change_image_resolution(pixel_width: int, pixel_length: int):
     x, y = pixel_width, pixel_length
     pass
 
+
+def cut_images(in_dir: Path,
+               out_dir: Path = None,
+               n: int = 144):
+    """Cut images in in_dir each into n smaller images. If n is a perfect square,
+    will cut into equal rows and columns.
+
+    :param in_dir: path to input image directory
+    :param out_dir: directory to save cut images
+    :param n: number of equal-sized images to cut out of each of the input images
+    """
+    if out_dir == None:
+        out_dir = in_dir
+    for jpg in sorted(in_dir.glob('*.jpg')):
+        slice(jpg, n)
+        if in_dir != out_dir:
+            # move all sub-pngs to out_dir
+            for png in sorted(in_dir.glob(jpg.stem + '_*.png')):
+                print(png)
+                name = png.name
+                png.replace(out_dir / name)
+
+
+def edit_geprint(lat: float, lon: float,
+                 template_dirpath: Path,
+                 save_image_quality: int=6,
+                 altitude: float=634.,
+                 template_filename: str='skybees_testconfig.geprint'):
+    """Edit the template Google Earth print config file for new lat-lon location.
+
+    :param lat: latitude of center of image to be downloaded
+    :param lon: longitude of center of image to be downloaded
+    :param save_image_quality: int to indicate image download resolution:
+        - 0 resolution of the window
+        - 1 1024x768
+        - 2 1280x720
+        - 3 1920x1080
+        - 4 3840x2160
+        - 5 8192x4320
+        - 6 4800x4800
+    :param altitude: emulated distance above ground of the image (basically how
+        far to zoom in so the image looks like it was taken from X feet above ground).
+    """
+
+
 # MAIN -------------------------------
 if __name__ == "__main__":
-    get_window()
+    # get_window()
 
+    _BEECENSUS_DIR = Path(*Path.cwd().parts[:Path.cwd().parts.index('beecensus') + 1])
+    _DATA_DIR = _BEECENSUS_DIR / 'data' / 'images'
+    cut_images(_DATA_DIR / 'high-altitude',
+               out_dir=_DATA_DIR / 'high-altitude-cut')
+    # TODO: finish edit_geprint, and download all images at lat-lon in new-sat-download-latlons.xlsx
+    # TODO: cut all images, sort into apiary and nonapiary
+    # TODO: twice as many images in train as validation
+    # TODO: run full training example, test how long it takes per image
+    # TODO: train fRNCC on legacy tfrecord images and see how long it takes per image
 
 
 
@@ -183,4 +240,8 @@ Must have been hidden in some way by the developers
                                     depth=3)  # search only for 1 level below
                                     
     dlg.child_window(control_type='Edit', top_level_only=False, visible_only=False).element_info.name
+    
+    
+NOTE ON GOOGLE EARTH IMAGE RIGHTS (from wiki, not actual source)    
+Google asserts that every image created from Google Earth using satellite data provided by Google Earth is a copyrighted map. Any derivative from Google Earth is made from data on which Google claims copyright under United States Copyright Law. Google grants licenses in this data allowing, among other things, non-commercial personal use of the images (e.g., on a personal website or blog) as long as copyrights and attributions are preserved.[17] 
 """
